eaa2c12d3ef7e31eba5f1b68b2c346295b3b51f6
Soverilus <30487703+Soverilus@users.noreply.github.com>
2018-06-05 13:30:02 +0800

    what the hell even happened

---
+++ Assets/MovementScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+public class MovementScript : MonoBehaviour {
+    Rigidbody2D playerRB;
+    public float maxVel;
+    Vector2 moveInput;
+    Vector2 moveInputRaw;
+    public float velMult;
+
+	void Start () {
+        playerRB = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+
+	}
+
+    private void FixedUpdate() {
+        moveInputRaw = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (SystemInfo.supportsAccelerometer) {
+            moveInput = new Vector2(Input.acceleration.x, Input.acceleration.y);
+        } else {
+            moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        }
+            playerRB.AddForce(moveInput * velMult);
+        if (playerRB.velocity.magnitude > maxVel) {
+            playerRB.AddForce(playerRB.velocity.normalized *- velMult);
+        }
+    }
+}
